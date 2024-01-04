2b130196e76ab2b8560353fee67e4c7941bf47c6
mr-ale141 <ale141@rambler.ru>
2024-01-04 07:56:47 +0300

    start composite data type

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@ template<typename T, size_t N>
 size_t size(const T (&data)[N]) { return N; }
 int main()
 {
-    int arr[3] = {20, 30, 40};
-    std::cout << sizeof(arr) / sizeof(arr[0]) << std::endl;
+    char str[] = {'A','B','C','D','E','F',32,'H','E','L','L','O'};
+    char arr[] = {70,71,72};
+    char str2[] = {'#','B','C','D','E','F',32,'H','E','L','L','!'};
+    std::cout << sizeof(str) / sizeof(str[0]) << std::endl;
     std::cout << size(arr) << std::endl;
+    std::cout << *arr << ' ' << arr << std::endl;
+    std::cout << str << std::endl;
+    std::cout << int(str[12]) << std::endl;
+    std::cout << int(str[13]) << std::endl;
+    std::cout << int(str[14]) << std::endl;
+    std::cout << str2 << std::endl;
     return 0;
 }
