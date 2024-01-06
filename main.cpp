dfddffddc04d6661ddbffa7a916e92fb5da75ecd
mr-ale141 <ale141@rambler.ru>
2024-01-06 10:05:53 +0300

    start intro in class string

--- main.cpp
+++ main.cpp
@@ -14,5 +14,17 @@ int main()
     std::cout << int(str[13]) << std::endl;
     std::cout << int(str[14]) << std::endl;
     std::cout << str2 << std::endl;
+    
+    int x;
+    char str3[32];
+
+    std::cout << "insert int: " << std::endl;
+    std::cin >> x;
+
+    std::cout << "insert string: " << std::endl;
+    std::cin.getline(str3, 32);
+
+    std::cout << x << ':' << str3 << std::endl;
+    std::cin >> x;
     return 0;
 }
