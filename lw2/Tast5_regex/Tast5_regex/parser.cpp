266edf0769d14c72c51170ebc9c544906048ee3b
mr-ale141 <ale141@rambler.ru>
2024-03-19 01:24:35 +0300

    lw2 regex fix port

--- lw2/Tast5_regex/Tast5_regex/parser.cpp
+++ lw2/Tast5_regex/Tast5_regex/parser.cpp
@@ -49,10 +49,10 @@ Protocol GetProtocol(const std::string& str)
 bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string& host, std::string& document)
 {
 	auto lowwerUrl = GetLowwer(url);
-	std::regex protocolR(R"(^(http[s]{0,1}|ftp))");
-	std::regex hostWithPortR(R"((\:\/\/([a-z0-9]+\.)+([a-z0-9]+)(\:[0-9]+)?\/))");
-	std::regex hostR(R"((([a-z0-9]+\.)+([a-z0-9]+)))");
-	std::regex portR(R"([0-9]+/$)");
+	const std::regex protocolR(R"(^(http[s]{0,1}|ftp))");
+	const std::regex hostWithPortR(R"((\:\/\/([a-z0-9]+\.)+([a-z0-9]+)(\:[0-9]+)?\/))");
+	const std::regex hostR(R"((([a-z0-9]+\.)+([a-z0-9]+)))");
+	const std::regex portR(R"(\:[0-9]+\/$)");
 
 	std::string protocolStr;
 	std::string hostWithPort;
@@ -75,7 +75,7 @@ bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string
 
 		if (std::regex_search(hostWithPort, result, hostR))
 		{
-			host = result.str(1);
+			host = result[0].str();
 		}
 		else
 		{
@@ -84,8 +84,13 @@ bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string
 
 		if (std::regex_search(hostWithPort, result, portR))
 		{
-			std::string portStr = result.str(1);
-			host = std::stoi(portStr);
+			std::string portStr = result[0].str();
+			portStr.erase(
+				std::remove_if(portStr.begin(), portStr.end(), 
+					[](char ch) { return ch == ':' || ch == '/'; }), 
+				portStr.end()
+			);
+			port = std::stoi(portStr);
 		}
 		else
 		{
@@ -97,7 +102,7 @@ bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string
 		return false;
 	}
 
-	int pos = protocolStr.length() + hostWithPort.length();
+	size_t pos = protocolStr.length() + hostWithPort.length();
 
 	document.clear();
 	document.append(url, pos, std::string::npos);
--- lw2/Tast5_regex/tests/tests.cpp
+++ lw2/Tast5_regex/tests/tests.cpp
@@ -17,7 +17,7 @@ TEST_CASE("Empty URL")
 	REQUIRE(!isOk);
 }
 
-TEST_CASE("Correct URL")
+TEST_CASE("Correct URL without PORT")
 {
 	const std::string url("http://www.mysite.com/docs/document1.html?page=30&lang=en#title");
 
@@ -33,4 +33,50 @@ TEST_CASE("Correct URL")
 	REQUIRE(port == 80);
 	REQUIRE(host == "www.mysite.com");
 	REQUIRE(document == "docs/document1.html?page=30&lang=en#title");
+}
+
+TEST_CASE("Correct URL with PORT")
+{
+	const std::string url("http://www.mysite.com:569/docs/document1.html?page=30&lang=en#title");
+
+	Protocol protocol;
+	int port;
+	std::string host;
+	std::string document;
+
+	auto isOk = ParseURL(url, protocol, port, host, document);
+
+	REQUIRE(isOk);
+	REQUIRE(protocol == Protocol::HTTP);
+	REQUIRE(port == 569);
+	REQUIRE(host == "www.mysite.com");
+	REQUIRE(document == "docs/document1.html?page=30&lang=en#title");
+}
+
+TEST_CASE("URL without protocol")
+{
+	const std::string url("www.mysite.com/docs/document1.html?page=30&lang=en#title");
+
+	Protocol protocol;
+	int port;
+	std::string host;
+	std::string document;
+
+	auto isOk = ParseURL(url, protocol, port, host, document);
+
+	REQUIRE(!isOk);
+}
+
+TEST_CASE("URL without host")
+{
+	const std::string url("http://mysite/docs/document1.html?page=30&lang=en#title");
+
+	Protocol protocol;
+	int port;
+	std::string host;
+	std::string document;
+
+	auto isOk = ParseURL(url, protocol, port, host, document);
+
+	REQUIRE(!isOk);
 }
