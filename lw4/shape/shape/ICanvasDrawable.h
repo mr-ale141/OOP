a783be5e5b27ddb473a8e7a664755c0b84213f70
ale141 <ale141@rambler.ru>
2024-04-11 01:18:14 +0300

    IBase start

--- lw4/shape/shape/ICanvasDrawable.h
+++ lw4/shape/shape/ICanvasDrawable.h
@@ -5,6 +5,20 @@ class ICanvasDrawable
 {
 public:
 	virtual void Draw(ICanvas canvas) = 0;
+	ICanvasDrawable(const ICanvasDrawable&) = delete;
+	ICanvasDrawable& operator=(const ICanvasDrawable&) = delete;
+
+	struct Deleter
+	{
+		void operator()(ICanvasDrawable* p) const { p->Delete(); }
+	};
+
 protected:
 	ICanvasDrawable() = default;
+	virtual ~ICanvasDrawable() = 0;
+	virtual void Delete();
 };
+
+ICanvasDrawable::~ICanvasDrawable() = default;
+
+void ICanvasDrawable::Delete() { delete this; }
